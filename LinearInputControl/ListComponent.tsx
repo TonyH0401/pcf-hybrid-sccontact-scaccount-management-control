a6ecbb3fe68927577b81539a3538f79847980ba9
Phu Le Gia <Phu.LeGia@selfstudy.store>
2025-04-23 10:48:41 +0100

    feat: create a table using fluentUI

--- LinearInputControl/ListComponent.tsx
+++ LinearInputControl/ListComponent.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { IInputs } from "./generated/ManifestTypes";
 
+import { DetailsList, IColumn, DetailsListLayoutMode } from "@fluentui/react";
+
 interface DummyData {
   id: number;
   name: string;
@@ -21,21 +23,42 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
     { id: 3, name: "Alice Johnson" },
     { id: 4, name: "Bob Brown" },
   ];
-  const title = context?.parameters?.sampleProperty.raw ?? "Unknown Title"
-  const Value = context?.parameters?.samplenNumber.raw ?? 100
+  const columns: IColumn[] = [
+    {
+      key: "column1",
+      name: "ID",
+      fieldName: "id",
+      minWidth: 50,
+      maxWidth: 100,
+      isResizable: true,
+    },
+    {
+      key: "column2",
+      name: "Tên",
+      fieldName: "name",
+      minWidth: 150,
+      maxWidth: 300,
+      isResizable: true,
+    },
+  ];
+  const title = context?.parameters?.sampleProperty.raw ?? "Unknown Title";
+  const value = context?.parameters?.sampleText.raw ?? "Unknown Value";
   return (
-    <div>
-      <h3>Danh sách {title}</h3>
-      <p>
-        Record ID: {Value}
-      </p>
-      <ul>
-        {dummyData.map((item) => (
-          <li key={item.id}>
-            ID: {item.id} - Tên: {item.name}
-          </li>
-        ))}
-      </ul>
+    <div style={{ padding: "16px" }}>
+      <div style={{ textAlign: "center", marginBottom: "20px" }}>
+        <h3 style={{ margin: 0 }}>Danh sách: {title}</h3>
+        <p style={{ margin: 0 }}>Record ID: {value}</p>
+      </div>
+
+      <div style={{ width: "100%", margin: "0 auto" }}>
+        <DetailsList
+          items={dummyData}
+          columns={columns}
+          setKey="set"
+          layoutMode={DetailsListLayoutMode.justified}
+          isHeaderVisible={true}
+        />
+      </div>
     </div>
   );
 };
