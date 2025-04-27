b5f54ecddd409c107dcf18e61374e9dcf20f1c51
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2025-04-27 23:45:13 +0700

    feat: home-add fetch to power auto for associate creation-add and divide section

--- LinearInputControl/ListComponent.tsx
+++ LinearInputControl/ListComponent.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+// import fetch from "node-fetch";
 import { IInputs } from "./generated/ManifestTypes";
 import {
   DetailsList,
@@ -10,15 +11,26 @@ import {
   PrimaryButton,
 } from "@fluentui/react";
 
-// Declare DummyData entity (to be deleted)
+// ============================================
+// Type Declarations Section
+// ============================================
+// Declare DummyData type (to be deleted)
 interface DummyData {
   id: number;
   name: string;
 }
+// Declare 
+// interface ScContact {
 
-// Used for passing data using context keyword
-// https://youtu.be/R1hTz-T5feQ?si=JAccsVjHru1K8hZl.
-// https://chatgpt.com/c/680890cf-ab18-8010-ad6e-d31757052e66. Scroll to the way top
+// }
+
+// ============================================
+// Component Input Interface Section (pass data using 'context' keyword)
+// ============================================
+/* 
+Video: https://youtu.be/R1hTz-T5feQ?si=JAccsVjHru1K8hZl.
+Chat: https://chatgpt.com/c/680890cf-ab18-8010-ad6e-d31757052e66. Scroll to the way top for the info
+*/
 interface ListComponentControlProps {
   context: ComponentFramework.Context<IInputs>;
   notifyOutputChanged: () => void;
@@ -49,7 +61,7 @@ async function fetchScAccountsData(
 Originally, it was "Fetch ScContact Data where ScContact IS associated with ScAccount via association table", I changed operator from 'eq' to 'ne' but it didn't work,
 it turned in to "Fetch ScContact Data where ScContact IS associated with OTHER ScAccount BUT NOT with the given ScAccount GUID via association table",
 so I need to find another way which is this way below. 
- */
+*/
 async function fetchScContactsDataAssociateNot(
   context: ComponentFramework.Context<IInputs>
 ) {
@@ -86,6 +98,14 @@ async function fetchScContactsDataAssociateNot(
     return [];
   }
 }
+// Post to PA
+// async function triggerRelateRowFlow(URL: string) {
+//   try {
+//     return [];
+//   } catch (error) {
+//     return [];
+//   }
+// }
 
 // ============================================
 // Main Component Section (this is where the magic begin)
@@ -101,7 +121,9 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
     { id: 3, name: "Alice Johnson" },
     { id: 4, name: "Bob Brown" },
   ];
-  // Initialize "state" to hold and set/change value
+  // ---------------------------
+  // State Variables (initialize "state" to hold and set/change value)
+  // ---------------------------
   const [searchText, setSearchText] = React.useState<string>(""); // State to hold "searchText" and "setSearchText", no initial value
   const [scAccounts, setScAccounts] = React.useState<unknown[]>([]); // State to hold "scAccounts" and "setScAccounts", no initial value
   const [scContacts, setScContacts] = React.useState<unknown[]>([]);
@@ -122,10 +144,13 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
     })
   );
 
+  // Run once when the page is render and run twice when context is updated (context is updated when the page is reloaded)
   React.useEffect(() => {
     const loadData = async () => {
+      setIsLoading(true);
       const contacts = await fetchScContactsDataAssociateNot(context);
       setScContacts(contacts);
+      setIsLoading(false);
     };
     loadData();
     // const loadData = async () => {
@@ -140,7 +165,10 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
     // loadData();
   }, [context]);
 
-  // 3. Compute filtered items whenever searchText changes
+  // ---------------------------
+  // Event Handlers
+  // ---------------------------
+  // Compute filtered items (with useMemo) whenever 'searchText' or 'scContacts' changes
   const filteredItems = React.useMemo(() => {
     const term = searchText.trim().toLowerCase();
     if (!term) return scContacts as unknown[];
@@ -154,43 +182,59 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
         contact.crff8_sccontactid?.toLowerCase().includes(term)
       );
     });
-  }, [searchText, scContacts]); // Dependency array, if any of these change, it trigger this, idk how to explain
-  //Event handler for button
+  }, [searchText, scContacts]); // Dependency array, if any of these variables change, it triggers this function, idk how to explain further
+  // Create (many-many) associate between ScAccount and ScContact via button click
   const handleGetSelectedId = async () => {
     const selectedItems = selection.getSelection();
+    // Throw an alert when the button is clicked with no selected row
     if (selectedItems.length === 0) {
       alert("Vui lòng chọn một dòng.");
       return;
     }
-    selectedItems.forEach((item, index) => {
-      const contact = item as {
-        crff8_sccontactid?: string;
-        crff8_sccontactnumber?: string;
-      };
-      console.log(
-        `Button Row ${index + 1}: ID - ${
-          contact.crff8_sccontactid
-        } and Number - ${contact.crff8_sccontactnumber}`
-      );
-    });
-    // 
+    const contactIds = selectedItems // Extract the GUID ONLY from the contact object
+      .map((item) => {
+        const contact = item as {
+          crff8_sccontactid?: string;
+          crff8_sccontactnumber?: string;
+        };
+        return contact.crff8_sccontactid;
+      })
+      .filter((id): id is string => !!id);
+    // Calling PA, careful because PA only has success cases
+    console.log("GUID: ", context?.parameters?.sampleText.raw);
+    console.log("Selected: ", contactIds);
+    setIsLoading(true);
     try {
-      await context.webAPI.createRecord("crff8_sccontact_crff8_scaccount", {
-          "crff8_SCAccountId@odata.bind": "/crff8_scaccounts(ad81f4bc-771e-f011-998a-000d3aa12cf4)",
-          "crff8_SCContactId@odata.bind": "/crff8_sccontacts(04e03b76-f720-f011-998a-000d3aa12cf4)"
-      });      
-      console.log("Success")
+      const URL =
+        "https://prod-93.southeastasia.logic.azure.com:443/workflows/df69e34664594b6ea57ad0c950ad0b00/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=BzQUHRqXsDKh2Qq8MukIbKCERx1V19x2Tiv6FRgHqOg";
+      const body = JSON.stringify({
+        account: context?.parameters?.sampleText.raw,
+        contact: contactIds,
+      });
+      const response = await fetch(URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: body,
+      });
+      const data = await response.json();
+      console.log("Success", data);
+      location.reload();
     } catch (error) {
-      console.log(error)
       console.error(`❌ Failed to link contact`, error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  // Define columns for DetailTable
+  // ---------------------------
+  // Render Components
+  // ---------------------------
+  // Define columns used in the component
   const columns: IColumn[] = [
     {
       key: "column1",
-      name: "ID", // Display name
+      name: "Number", // Display name
       fieldName: "crff8_sccontactnumber", // This is where the data is mapped based on the column name/logical name
       minWidth: 50,
       maxWidth: 100,
@@ -198,7 +242,7 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
     },
     {
       key: "column2",
-      name: "Tên",
+      name: "GUID",
       fieldName: "crff8_sccontactid", // This is where the data is mapped based on the column name/logical name
       minWidth: 150,
       maxWidth: 300,
@@ -208,39 +252,85 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
 
   const title = context?.parameters?.sampleProperty.raw ?? "Unknown Title";
   const value = context?.parameters?.sampleText.raw ?? "Unknown Value";
-
+  // The "loading overlay" component
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(255, 255, 255, 0.7)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 9999,
+        }}
+      >
+        <div
+          style={{
+            border: "6px solid #f3f3f3",
+            borderTop: "6px solid #3498db",
+            borderRadius: "50%",
+            width: "50px",
+            height: "50px",
+            animation: "spin 1s linear infinite",
+          }}
+        />
+        <p>Loading...</p>
+        <style>{`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}</style>
+      </div>
+    );
+  }
+  // Main component
   return (
-    //
     <div style={{ padding: "16px" }}>
+      {/* Header */}
       <div style={{ textAlign: "center", marginBottom: "20px" }}>
         <h3 style={{ margin: 0 }}>Danh sách: {title}</h3>
         <p style={{ margin: 0 }}>Record ID: {value}</p>
       </div>
-
+      {/* Search box */}
       <div style={{ width: "100%", maxWidth: "600px", margin: "0 auto 12px" }}>
         <SearchBox
           placeholder="Tìm theo ID hoặc tên..."
           value={searchText} // This is for displaying UI only
-          onChange={(_, newValue) => setSearchText(newValue || "")} // When the user change any values, it will update searchText
+          onChange={(_, newValue) => setSearchText(newValue || "")} // When the user change any values, it will update 'searchText'
           underlined={false}
         />
       </div>
-
-      <div style={{ width: "100%", margin: "0 auto", height: '300px', overflowY: 'auto' }}>
+      {/* Display list */}
+      <div
+        style={{
+          width: "100%",
+          margin: "0 auto",
+          height: "300px",
+          overflowY: "auto",
+        }}
+      >
         <DetailsList
           items={filteredItems} // Instead of dummy value, it will load based on filteredItems from the search
           columns={columns} // Used to columns we defined before
           setKey="filtered"
           layoutMode={DetailsListLayoutMode.justified}
           isHeaderVisible={true}
-          selection={selection}
+          selection={selection} // Enable selection of table trigger everytime there is a 'selection' state
           selectionPreservedOnEmptyClick={true}
           selectionMode={SelectionMode.multiple}
         />
       </div>
-
       {/* Button */}
-      <div style={{ textAlign: "center", marginBottom: "12px" }}>
+      <div
+        style={{ textAlign: "center", marginTop: "12px", marginBottom: "12px" }}
+      >
         <PrimaryButton
           text="Lấy ID từ dòng được chọn"
           onClick={handleGetSelectedId}
@@ -250,4 +340,7 @@ const ListComponentControl: React.FC<ListComponentControlProps> = ({
   );
 };
 
+// ============================================
+// Export Component
+// ============================================
 export default ListComponentControl;
